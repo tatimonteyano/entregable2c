75f913c6f6233db93fc36e6772afe9836e09a5c8
tatiana <tatianamonteyano27@gmail.com>
2023-11-06 15:25:36 -0300

    agregando fileSystem

--- app.js
+++ app.js
@@ -1,46 +1,86 @@
+const fs = require("fs"); //import
+
 // Inicializa products como un arreglo vacío.
 class ProductManager {
-   constructor() {
-     this.products =[];
-     this.id = 0; // Propiedad de instancia para el ID
-   }
-//agrega un producto
-addProduct (title, description, price, thumbnail ="https://acortar.link/EzwwQl", code, stock){
-         //id autoincrementable
-         this.id++
-        // Validación de propiedades
-      if (!title || !description || price === undefined || code === undefined || stock === undefined) {
-          console.log("Faltan propiedades o algunas son inválidas.");
-          return;
-        }
-        // Validación de código no repetido
-        const codeExists = this.isCodeRepeated(code);
-        if (codeExists) {
-          console.log("El código ya está en uso para otro producto.");
-          return;
-        }
-     this.products.push({title, description, price, thumbnail, code, stock, id:this.id});
-   };
-isCodeRepeated(code) {
-   return this.products.some((product) => product.code === code);
-}
-//agregar metodo getProducts
-getProducts() {
-       return this.products;
-};
-//get product by id
-getProductById(id) {
-      const product = this.products.find((product) => product.id === id);
-      if (!product) {
-         return "Not found"; // Devuelve un mensaje en lugar de imprimirlo
+  constructor(fileName) {
+    this.id = 0;
+    this.path = fileName;
+   //aqui iba un this.products = []
+    if (fs.existsSync(this.path)) {
+      try {
+        let productFile = fs.readFileSync(this.path, "utf-8");
+        this.products = JSON.parse(productFile);
+      } catch (error) {
+        this.products = [];
       }
-      return product;
-   }; 
-};
+    } else {
+      this.products = [];
+   }
+  }
 
-const products = new ProductManager();
-products.addProduct("Bolsa de alimento", "bolsa de alimento de 15kg, Milhosky", 35000, "https://acortar.link/EzwwQl",253, 50);
-products.addProduct("Bolsa de alimento", "bolsa de alimento de 15kg, Benikosky", 38000, "https://acortar.link/EzwwQl",254, 20);
-products.addProduct("Bolsa de alimento", "bolsa de alimento de 15kg, Garfieldsky", 40000, "https://acortar.link/EzwwQl", 254, 20); //El código ya está en uso para otro producto.
-console.log(products.getProducts());
-products.getProductById (8);
+  //agrega un producto
+  async addProduct(title, description, price, thumbnail, code, stock) {
+    //id autoincrementable
+    this.id++;
+    // Validación de propiedades
+    if (
+      !title ||
+      !description ||
+      price === undefined ||
+      code === undefined ||
+      stock === undefined
+    ) {
+      console.log("Faltan propiedades o algunas son inválidas.");
+      return;
+    }
+    // Validación de código no repetido
+    const codeExists = this.isCodeRepeated(code);
+    if (codeExists) {
+      console.log("El código ya está en uso para otro producto.");
+      return;
+    }
+    this.products.push({
+      title,
+      description,
+      price,
+      thumbnail,
+      code,
+      stock,
+      id: this.id,
+    });
+    await this.saveFileProducts();
+  }
+
+  isCodeRepeated(code) {
+    return this.products.some((product) => product.code === code);
+  }
+
+  //agregar metodo getProducts
+  getProducts() {
+    return this.products;
+  }
+
+  async saveFileProducts (){
+      try {
+        await fs.promises.writeFile(
+          this.path,
+          JSON.stringify(this.products, null, "\t")
+        );
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+  };
+
+  //get product by id
+  getProductById(id) {
+    const product = this.products.find((product) => product.id === id);
+    if (!product) {
+      return "Not found"; // Devuelve un mensaje en lugar de imprimirlo
+    }
+    return product;
+  }
+}
+const productManagerTest = new ProductManager("./pruebas.txt"); // instancia de la clase
+console.log(productManagerTest.getProducts());
